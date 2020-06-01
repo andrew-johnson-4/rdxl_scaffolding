2e8d1a2ff883d786a15aaf0b6a2dc0fd567550bf
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-05-31 18:40:44 -0700

    first end-to-end integration of rdxl passing now

--- src/html.rs
+++ src/html.rs
@@ -2,5 +2,7 @@ use std::fmt;
 use rdxl::xrender;
 use crate::*;
 
-xrender!(ProgressBar, <div style="position:relative; height:30px; width:200px; background-color: #CCCCCC;">
+xrender!(ProgressBar, <div style="position:relative; height:30px; width:300px; background-color:#CCCCCC;">
+  <div style={{ format!("\"position:absolute; top:0; left:0; height:30px; width:{}px; background-color:#999999;\"", self.numerator*300/self.denominator ) }}></div>
+  <div style="line-height:30px; width:300px; text-align:center; font-family:sans;">{{ self.numerator }} / {{ self.denominator }} {{ self.unit }}</div>
 </div>);
