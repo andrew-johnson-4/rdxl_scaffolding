0299b33c11996ee6380023a78fa10c5df5ce51a6
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-05-31 20:23:28 -0700

    add lots of elements

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,20 @@ pub mod html;
 
 xtype!(<!ProgressBar numerator:u64 denominator:u64 unit:String/>);
 xtype!(<!IndexTabs><!IndexTab name:String/></IndexTabs>);
+
+xtype!(<!Person name:Name/>);
+xtype!(<!Name name:String/>);
+xtype!(<!Title title:String/>);
+xtype!(<!Email email:String/>);
+xtype!(<!PhoneNumber number:String/>);
+xtype!(<!Website url:String/>);
+xtype!(<!Contact person:Person>
+   <?Title/>
+   <?Email/>
+   <?PhoneNumber/>
+   <?Website/>
+</Contact>);
+
+xtype!(<!Date year:u64 month:u64 day:u64/>);
+xtype!(<!Time hour:u64 minute:u64 second:u64/>);
+xtype!(<!DateTime date:Date time:Time/>);
