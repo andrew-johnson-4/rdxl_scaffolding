c62ce72422cb63fb496cb73b6355ae60c61a42c5
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-04 11:21:58 -0700

    add Image xtyep

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,7 @@ xtype!(<!InputTime name:String/>);
 xtype!(<!InputUrl name:String/>);
 xtype!(<!InputWeek name:String/>);
 
+xtype!(<!Image name:String/>);
 xtype!(<!Icon name:String/>);
 
 xtype!(<!Person name:Name/>);
