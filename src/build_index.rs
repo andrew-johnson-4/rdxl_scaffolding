02085adb14b2c777c4ffc81da67b1266584ace57
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-07 12:29:42 -0700

    use bootstrap css for example

--- src/build_index.rs
+++ src/build_index.rs
@@ -8,6 +8,9 @@ fn main() -> std::io::Result<()> {
    let mut f = File::create("index.html")?;
 
    f.write_all(b"<html>")?;
+   f.write_all(b"<head>")?;
+   f.write_all(br#"<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">"#)?;
+   f.write_all(b"</head>")?;
    f.write_all(b"<body>")?;
 
    f.write_all(xhtml!( <!IndexTabs>
