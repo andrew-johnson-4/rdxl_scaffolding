68fc3118a2ec76cdbf4233b79e6c539f298c96b5
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-25 13:17:52 -0700

    html demands the notion of *optional* attributes

--- src/dom.rs
+++ src/dom.rs
@@ -1,4 +1,4 @@
-use rdxl::{xtype};
+use rdxl::{xtype,xrender};
 
 xtype!(
    <!Script async_:bool={{false}} charset:String={{"UTF-8".to_string()}} defer:bool={{false}}
@@ -6,6 +6,12 @@ xtype!(
      <?/>
    </Script>
 );
+xrender!(Script,<script charset={{self.charset}} src={{self.src}} type={{self.type_}}>
+   {{ for cd in self.children.iter() {{
+      {{ let ScriptChildren::Display(cd) = cd; }}
+      {{cd}}
+   }} }}
+</script>);
 
 xtype!(
   <!Html lang:String={{"en".to_string()}} doctype:String={{"html".to_string()}} xmlns:String={{"".to_string()}}>
