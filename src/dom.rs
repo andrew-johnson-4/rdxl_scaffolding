a12c6a7bcc7f8cbe8ed6977fe56bd6fc3bc476b1
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-25 14:57:22 -0700

    integrate new rdxl features

--- src/dom.rs
+++ src/dom.rs
@@ -6,7 +6,9 @@ xtype!(
      <?/>
    </Script>
 );
-xrender!(Script,<script charset={{self.charset}} src={{self.src}} type={{self.type_}}>
+xrender!(Script,<script {{if self.async_ {{async}}}} charset={{self.charset}}
+                        {{if self.defer {{defer}}}}
+                        src={{self.src}} type={{self.type_}}>
    {{ for cd in self.children.iter() {{
       {{ let ScriptChildren::Display(cd) = cd; }}
       {{cd}}
